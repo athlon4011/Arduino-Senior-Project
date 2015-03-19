a0f6621fbec1f8d5c44456b560d94564c9f08ed2
jesus44 <ljesus289@gmail.com>
2015-03-19 12:55:28 -0700

    Revert "Event_Log / Recv_timeout function"
    This reverts commit 7a7cf3b6c10178ba7c53a788c840156f90f7b79e.

--- RaspPi/home/pi/scripts/Homer_OnBoot_Python.py
+++ RaspPi/home/pi/scripts/Homer_OnBoot_Python.py
@@ -22,14 +22,13 @@ def convert_String_ToJSON( arduinoData ):
     except (ValueError, KeyError, TypeError):
         print ("JSON ERROR")
 
+
 #Store JSON into Database
 def dBJSON_Store(json_file,NodeID):
     cursor = dataBase.cursor()
     cursor.execute("Insert into Sensor_Log(Date,NodeID,Data) values(NOW(),%s,%s)",(NodeID,json_file))
 
     dataBase.commit()
-
-    return "Store"
 
 #Send Request to Arduino for Sensor information
 def arduino_Data_Request(NodeIP):
@@ -41,8 +40,7 @@ def arduino_Data_Request(NodeIP):
     # As you can see, there is no connect() call; UDP has no connections.
     # Instead, data is directly sent to the recipient via sendto().
     sock.sendto("pull" + "\n", (HOST, PORT))
-    received = recv_timeout(sock)
-    print(received)
+    received = sock.recv(1024)
     return received;
 
 #Fetch an Associative Array from MySQL database
@@ -67,74 +65,25 @@ def mysql_fetch_assoc():
 
 #Checks to see if the last data entry matches if so, do not store in Database
 def check_duplicate_data(NodeID,json_file):
-
     try:
         cursor = dataBase.cursor()
-        cursor.execute("Select count(*) from Sensor_Log")
-        count =  cursor.fetchone()[0]
-        if count == 0:
-            return False
+        cursor.execute("Select Data from Sensor_Log where NodeID = %s ORDER BY Date DESC LIMIT 1",(NodeID))
+        previous_data = ''.join(cursor.fetchone())
+        if previous_data == json_file:
+            return True
         else:
-            cursor.execute("Select Data from Sensor_Log where NodeID = %s ORDER BY Date DESC LIMIT 1",(NodeID))
-            previous_data = ''.join(cursor.fetchone())
-            if previous_data == json_file:
-                return True
-            else:
-                return False
+            return False
     except Exception as err:
         print("Error checking duplicate data")
         print(err)
 
-def log_Event(type,NodeID):
-    cursor = dataBase.cursor()
-    
-    if type == "Store":
-        message = "Node: " + str(NodeID) + " Sensor Data was stored." 
-        cursor.execute("Insert into Event_Log(Date,Type,Message) values(NOW(),%s,%s)",(type,message))
-        dataBase.commit()
-
-def recv_timeout(the_socket,timeout=2):
-    #make socket non blocking
-    the_socket.setblocking(0)
-     
-    #total data partwise in an array
-    total_data=[];
-    data='';
-     
-    #beginning time
-    begin=time.time()
-    while 1:
-        #if you got some data, then break after timeout
-        if total_data and time.time()-begin > timeout:
-            break
-         
-        #if you got no data at all, wait a little longer, twice the timeout
-        elif time.time()-begin > timeout*2:
-            break
-         
-        #recv something
-        try:
-            data = the_socket.recv(8192)
-            if data:
-                total_data.append(data)
-                #change the beginning time for measurement
-                begin=time.time()
-            else:
-                #sleep for sometime to indicate a gap
-                time.sleep(0.1)
-        except:
-            pass
-     
-    #join all parts to make final string
-    return ''.join(total_data)    
-    
 #Runs Forever
 while True:
     for node in mysql_fetch_assoc():
         arduinoData = arduino_Data_Request(node['IP'])
+        print(arduinoData)
         json_file = convert_String_ToJSON(arduinoData)
         if check_duplicate_data(node['NodeID'],json_file) == False:
-            type = dBJSON_Store(json_file,node['NodeID']);
-            log_Event(type,node['NodeID'])
-        time.sleep(1)
+            dBJSON_Store(json_file,node['NodeID']);
+        time.sleep(5)
   
