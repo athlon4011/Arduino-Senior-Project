419d4285098f288200a0335ffa83963d05977776
athlon4011 <athlon4011@gmail.com>
2015-04-19 21:56:55 -0700

    setLED fix

--- Arduino/Node/separateData.ino
+++ Arduino/Node/separateData.ino
@@ -1,4 +1,4 @@
 void separateData(String message) {
   setTone(int(message[0])-48);
-  setLED((message.substring(2,5)).toInt(), (message.substring(6,9)).toInt(), (message.substring(10,13)).toInt(), int(message[14])-48, int(message[16])-48, int(message[18])-48);
+  setLED((message.substring(2,5)).toInt(), (message.substring(6,9)).toInt(), (message.substring(10,13)).toInt(), int(message[14])-48, int(message[16])-48);
 }
