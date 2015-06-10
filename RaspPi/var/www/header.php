d92cdcced619982ed7779add12cd554e1b3befa1
Mluck022 <mluck022@gmail.com>
2015-06-09 20:02:20 -0700

    File Changes

--- RaspPi/var/www/header.php
+++
@@ -1,8 +0,0 @@
-<div class="container header">
-            <label style="font-size:xx-large">H.O.M.E.R</label>
-            <ul id="myTab" class="nav nav-tabs" style="position:absolute; bottom:0;left:0;">
-                <li class="nav active"><a href="#Home" data-toggle="tab">Home</a></li>
-                <li class="nav"><a href="#Rules" data-toggle="tab">Rules Engine</a></li>
-                <li class="nav"><a href="#House" data-toggle="tab">Simulated House</a></li>
-            </ul>
-        </div>
