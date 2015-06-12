40db265d44db2a9803e93990676bab5806544a40
athlon4011 <athlon4011@gmail.com>
2015-06-11 23:00:20 -0700

    Simulated Smoke state

--- Arduino/Node/getSmoke.ino
+++ Arduino/Node/getSmoke.ino
@@ -1,5 +1,10 @@
 String getSmoke() {
- String temp = "";
-  
+String temp;
+  if(debug) {
+    Serial.print("Smoke Alarm state: ");
+    //Serial.println(digitalRead(motion_pin));
+  }
+  //temp += "motion"; temp += ','; temp += digitalRead(motion_pin); temp += ',';
+  temp += "smoke"; temp += ','; temp += '0'; temp += ',';
   return temp;
 }
