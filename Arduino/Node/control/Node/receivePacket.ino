9d27fbc3f408aa40eaf5fc12d34c009ddb20e308
atlon4011 <athlon4011@gmail.com>
2015-06-16 13:17:56 -0700

    Clean and commented control node source

--- Arduino/Node/control/Node/receivePacket.ino
+++ Arduino/Node/control/Node/receivePacket.ino
@@ -1,23 +1,24 @@
 void receivePacket() {
+	// Receive and process a UDP message
   int packetSize = Udp.parsePacket();
   if(packetSize)
   {
     IPAddress remote = Udp.remoteIP();
-    if(server == remote) {
+    if(server == remote) {	// Validate the IP the message is coming from
       // read the packet into packetBufffer
       Udp.read(packetBuffer,UDP_TX_PACKET_MAX_SIZE);
       Serial.print("packetsize:");
       Serial.println(packetSize);
       Serial.print("Contents:");
       Serial.println(packetBuffer);
-      if(String(packetBuffer).startsWith("send")) {
+      if(String(packetBuffer).startsWith("send")) { // Validate the message type
+        // Separate the message into usable data
         String test = String(packetBuffer);
         test.remove(0,5);
         Serial.print("server said: ");
         Serial.println(test);
         control(int(test[0])-48,int(test[2])-48);
       }
-      //return String(packetBuffer);
     }
   }
 }
