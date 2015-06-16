32a54f028c071e2ab74ab0431bf7d14d651f2e73
athlon4011 <athlon4011@gmail.com>
2015-06-16 06:34:43 -0700

    Node cleanup part 1

--- Arduino/Node/data/Node/extTemp.ino
+++ Arduino/Node/data/Node/extTemp.ino
@@ -1,14 +1,7 @@
 String extTemp() {
+  // Gets the temperature reading from the lm34 and combines it into a csv
   String temp;
-   //sensor_reading = analogRead(sensor_pin);         // stores the digitized (0 - 1023) analog reading from the LM34
-   fahrenheit = (100.0 * analogRead(temp_pin) * vref)/1023;   // calculates the actual fahrenheit temperature
-   //centigrade = (((5.0/9.0))*(fahrenheit-32.0)); //conversion to degrees C
-   //temp += centigrade; temp += ',';
-   temp += "temp"; temp += ','; temp += fahrenheit; temp += ',';
-   /*if(debug) {
-     Serial.print("External Temperature: ");
-     Serial.print (centigrade); Serial.print("C "); 
-     Serial.print(fahrenheit); Serial.println("F ");
-   }*/
-   return temp;
+  fahrenheit = (100.0 * analogRead(temp_pin) * vref)/1023;   // calculates the actual fahrenheit temperature
+  temp += "temp"; temp += ','; temp += fahrenheit; temp += ',';
+  return temp;
 }
